d64bca9ded7f02cc1864d69b89c3aa8af309c8a3
Ethan Johnson <ethan.t.johnson1417@gmail.com>
2021-10-13 16:01:13 -0600

    Corrections

--- app.js
+++ app.js
@@ -40,13 +40,13 @@ function rps(playerChoice) {
     console.log('player picked:', playerChoice, 'computer picked: ', compChoice)
     if (playerChoice == 'paper' && compChoice.name == "paper") {
         document.getElementById('message').innerText = 'You tied!'
-        document.getElementById('Pimg').innerHTML = `<img class="img-fluid rounded imgsz" src="http://media.gq.com/photos/5965abe1ae316b79758765c4/16:9/pass/the-rock-skin.jpg" alt="Rock">`
-        document.getElementById('Cimg').innerHTML = `<img class="img-fluid rounded imgsz" src="http://media.gq.com/photos/5965abe1ae316b79758765c4/16:9/pass/the-rock-skin.jpg" alt="Rock">`
+        document.getElementById('Pimg').innerHTML = `<img class="img-fluid rounded imgsz" src="https://www.boothbayregister.com/sites/default/files/2016/01/field/image/us-constitution.jpg" alt="Paper">`
+        document.getElementById('Cimg').innerHTML = `<img class="img-fluid rounded imgsz" src="https://www.boothbayregister.com/sites/default/files/2016/01/field/image/us-constitution.jpg" alt="Paper">`
     }
     if (playerChoice == 'scissors' && compChoice.name == "scissors") {
         document.getElementById('message').innerText = 'You tied!'
-        document.getElementById('Pimg').innerHTML = `<img class="img-fluid rounded imgsz" src="http://media.gq.com/photos/5965abe1ae316b79758765c4/16:9/pass/the-rock-skin.jpg" alt="Rock">`
-        document.getElementById('Cimg').innerHTML = `<img class="img-fluid rounded imgsz" src="http://media.gq.com/photos/5965abe1ae316b79758765c4/16:9/pass/the-rock-skin.jpg" alt="Rock">`
+        document.getElementById('Pimg').innerHTML = `<img class="img-fluid rounded imgsz" src="https://www.thegreenhead.com/imgs/japanese-samurai-katana-sword-scissors-9.jpg" alt="Scissors">`
+        document.getElementById('Cimg').innerHTML = `<img class="img-fluid rounded imgsz" src="https://www.thegreenhead.com/imgs/japanese-samurai-katana-sword-scissors-9.jpg" alt="Scissors">`
     }
     if (playerChoice == 'rock' && compChoice.name == "rock") {
         document.getElementById('message').innerText = 'You tied!'
@@ -62,30 +62,30 @@ function rps(playerChoice) {
     }
     if (playerChoice == 'rock' && compChoice.name == "scissors") {
         document.getElementById('message').innerText = 'You Win!'
-        document.getElementById('Pimg').innerHTML = `<img class="img-fluid rounded imgsz" src="https://www.boothbayregister.com/sites/default/files/2016/01/field/image/us-constitution.jpg" alt="Paper">`
-        document.getElementById('Cimg').innerHTML = `<img class="img-fluid rounded imgsz" src="http://media.gq.com/photos/5965abe1ae316b79758765c4/16:9/pass/the-rock-skin.jpg" alt="Rock">`
+        document.getElementById('Pimg').innerHTML = `<img class="img-fluid rounded imgsz" src="http://media.gq.com/photos/5965abe1ae316b79758765c4/16:9/pass/the-rock-skin.jpg" alt="Rock">`
+        document.getElementById('Cimg').innerHTML = `<img class="img-fluid rounded imgsz" src="https://www.thegreenhead.com/imgs/japanese-samurai-katana-sword-scissors-9.jpg" alt="Scissors">`
     }
     if (playerChoice == 'scissors' && compChoice.name == "paper") {
         document.getElementById('message').innerText = 'You Win!'
-        document.getElementById('Pimg').innerHTML = `<img class="img-fluid rounded imgsz" src="https://www.boothbayregister.com/sites/default/files/2016/01/field/image/us-constitution.jpg" alt="Paper">`
-        document.getElementById('Cimg').innerHTML = `<img class="img-fluid rounded imgsz" src="http://media.gq.com/photos/5965abe1ae316b79758765c4/16:9/pass/the-rock-skin.jpg" alt="Rock">`
+        document.getElementById('Pimg').innerHTML = `<img class="img-fluid rounded imgsz" src="https://www.thegreenhead.com/imgs/japanese-samurai-katana-sword-scissors-9.jpg" alt="Scissors">`
+        document.getElementById('Cimg').innerHTML = `<img class="img-fluid rounded imgsz" src="https://www.boothbayregister.com/sites/default/files/2016/01/field/image/us-constitution.jpg" alt="Paper">`
     }
 
 
     if (playerChoice == 'scissors' && compChoice.name == "rock") {
-        document.getElementById('message').innerText = "You Suck..."
-        document.getElementById('Pimg').innerHTML = `<img class="img-fluid rounded imgsz" src="https://www.thegreenhead.com/imgs/japanese-samurai-katana-sword-scissors-9.jpg" alt="Rock">`
+        document.getElementById('message').innerText = "You lose..."
+        document.getElementById('Pimg').innerHTML = `<img class="img-fluid rounded imgsz" src="https://www.thegreenhead.com/imgs/japanese-samurai-katana-sword-scissors-9.jpg" alt="Scissors">`
         document.getElementById('Cimg').innerHTML = `<img class="img-fluid rounded imgsz" src="http://media.gq.com/photos/5965abe1ae316b79758765c4/16:9/pass/the-rock-skin.jpg" alt="Rock">`
     }
     if (playerChoice == 'rock' && compChoice.name == "paper") {
-        document.getElementById('message').innerText = "You Suck..."
-        document.getElementById('Pimg').innerHTML = `<img class="img-fluid rounded imgsz" src="https://www.thegreenhead.com/imgs/japanese-samurai-katana-sword-scissors-9.jpg" alt="Rock">`
-        document.getElementById('Cimg').innerHTML = `<img class="img-fluid rounded imgsz" src="http://media.gq.com/photos/5965abe1ae316b79758765c4/16:9/pass/the-rock-skin.jpg" alt="Rock">`
+        document.getElementById('message').innerText = "You lose..."
+        document.getElementById('Pimg').innerHTML = `<img class="img-fluid rounded imgsz" src="http://media.gq.com/photos/5965abe1ae316b79758765c4/16:9/pass/the-rock-skin.jpg" alt="Rock">`
+        document.getElementById('Cimg').innerHTML = `<img class="img-fluid rounded imgsz" src="https://www.boothbayregister.com/sites/default/files/2016/01/field/image/us-constitution.jpg" alt="Paper">`
     }
     if (playerChoice == 'paper' && compChoice.name == "scissors") {
-        document.getElementById('message').innerText = "You Suck..."
-        document.getElementById('Pimg').innerHTML = `<img class="img-fluid rounded imgsz" src="https://www.thegreenhead.com/imgs/japanese-samurai-katana-sword-scissors-9.jpg" alt="Rock">`
-        document.getElementById('Cimg').innerHTML = `<img class="img-fluid rounded imgsz" src="http://media.gq.com/photos/5965abe1ae316b79758765c4/16:9/pass/the-rock-skin.jpg" alt="Rock">`
+        document.getElementById('message').innerText = "You lose..."
+        document.getElementById('Pimg').innerHTML = `<img class="img-fluid rounded imgsz" src="https://www.boothbayregister.com/sites/default/files/2016/01/field/image/us-constitution.jpg" alt="Paper">`
+        document.getElementById('Cimg').innerHTML = `<img class="img-fluid rounded imgsz" src="https://www.thegreenhead.com/imgs/japanese-samurai-katana-sword-scissors-9.jpg" alt="Scissors">`
     }
 }
 
